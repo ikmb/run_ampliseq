5aae4c4a33df67cd34d64ab057bb40133f0a4fdf
brovolia <brov.olia@gmail.com>
2026-06-24 10:04:23 +0200

    rewrite pipeline command

--- run_ampliseq_gmbc_18s.sh
+++ run_ampliseq_gmbc_18s.sh
@@ -12,29 +12,9 @@ module load gcc12-env
 module load nextflow
 module load singularity
 
-# 18S primers
-FW_PRIMER="TTAAARVGYTCGTAGTYG"
-RV_PRIMER="CCGTCAATTHCTTYAART"
-
 nextflow run nf-core/ampliseq \
   -r 2.16.1 \
   -profile ccga_cau \
   -c ampliseq_custom_18s.config \
-  -resume \
-  --input samplesheet.tsv \
-  --outdir results_filtered_18s \
-  --FW_primer "${FW_PRIMER}" \
-  --RV_primer "${RV_PRIMER}" \
-  --skip_cutadapt \
-  --dada_assign_taxlevels Domain,Supergroup,Division,Subdivision,Class,Order,Family,Genus,Species \
-  --ignore_failed_filtering \
-  --trunclenf 250 \
-  --trunclenr 210 \
-  --max_ee 2 \
-  --skip_dada_addspecies \
-  --ignore_empty_input_files \
-  --db pr2 \
-  --min_samples 2 \
-  --min_frequency 10 \
-  --skip_diversity_indices \
-  --metadata metadata.tsv
+  -params-file params_18s.yml \
+  -resume
--- run_ampliseq_gmbc_v3v4.sh
+++ run_ampliseq_gmbc_v3v4.sh
@@ -12,27 +12,9 @@ module load gcc12-env
 module load nextflow
 module load singularity
 
-# V3-V4 primers
-FW_PRIMER="CCTACGGGAGGCAGCAG"
-RV_PRIMER="GGACTACHVGGGTWTCTAAT"
-
 nextflow run nf-core/ampliseq \
   -r 2.16.1 \
   -profile ccga_cau \
   -c ampliseq_custom.config \
-  -resume \
-  --input samplesheet.tsv \
-  --outdir results_filtered \
-  --FW_primer "${FW_PRIMER}" \
-  --RV_primer "${RV_PRIMER}" \
-  --dada_assign_taxlevels Kingdom,Phylum,Class,Order,Family,Genus,Species \
-  --ignore_failed_filtering \
-  --trunclenf 260 \
-  --trunclenr 200 \
-  --max_ee 2 \
-  --ignore_empty_input_files \
-  --dada_ref_taxonomy gtdb \
-  --min_samples 2 \
-  --min_frequency 10 \
-  --skip_diversity_indices \
-  --metadata metadata.tsv
+  -params-file params_v3v4.yml \
+  -resume
